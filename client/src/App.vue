fa14a6dfa7b2d1b9dcab89aa1cae09b9c126d2ac
ardhptr21 <ardhiputrapradana21@gmail.com>
2022-08-13 11:09:47 +0700

    client: setup client vue js and tailwind css

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <h1>My Unsplash</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style></style>
